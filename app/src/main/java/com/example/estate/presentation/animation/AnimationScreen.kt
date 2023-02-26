88b6a878e9174b6221908e0accebfee5a9ea05af
BENJAH <benjahjp@gmail.com>
2023-02-26 17:50:18 +0300

    first commit

--- app/src/main/java/com/example/estate/presentation/animation/AnimationScreen.kt
+++ app/src/main/java/com/example/estate/presentation/animation/AnimationScreen.kt
@@ -3,6 +3,7 @@ package com.example.estate.presentation.animation
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.media3.ui.PlayerView
@@ -21,13 +23,16 @@ import com.example.estate.ui.theme.BlackSurface
 fun AnimationScreen(
     viewModel: AnimationViewModel = hiltViewModel()
 ) {
-    Scaffold() {
+    Scaffold(
+        containerColor = Color.White
+    ) {
         Box(
             modifier = Modifier.fillMaxSize()
-                .background(color = BlackSurface)
         ) {
             AndroidView(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxWidth()
+                    .aspectRatio(16 / 9f)
+                ,
                 factory = { context ->
                     PlayerView(context).also {
                         it.player = viewModel.player
